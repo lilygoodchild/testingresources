011a20955c5717a4c4b395eba2e11f5b6d991caf
Lily <lsmgoodchild@icloud.com>
2023-08-02 15:01:58 +0100

    Initial Commit

---
+++ testingresources/testingresourcesApp.swift
@@ -0,0 +1,17 @@
+//
+//  testingresourcesApp.swift
+//  testingresources
+//
+//  Created by scholar on 02/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct testingresourcesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
